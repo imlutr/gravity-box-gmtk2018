c7d031c10f73fe9b9af237427675bccf8b540980
Luca1152 <t.luca1152@gmail.com>
2018-09-01 13:49:37 +0300

    Set the window size to 600x600 and enabled anti aliasing.

--- desktop/src/ro/luca1152/gmtk/desktop/DesktopLauncher.java
+++ desktop/src/ro/luca1152/gmtk/desktop/DesktopLauncher.java
@@ -2,11 +2,18 @@ package ro.luca1152.gmtk.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+
 import ro.luca1152.gmtk.MyGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "GMTK 2018";
+        config.width = 600;
+        config.height = 600;
+        config.initialBackgroundColor = Color.WHITE;
+        config.samples = 4;
+        new LwjglApplication(new MyGame(), config);
+    }
 }
--- html/src/ro/luca1152/gmtk/client/HtmlLauncher.java
+++ html/src/ro/luca1152/gmtk/client/HtmlLauncher.java
@@ -3,49 +3,19 @@ package ro.luca1152.gmtk.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
 import ro.luca1152.gmtk.MyGame;
 
 public class HtmlLauncher extends GwtApplication {
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        GwtApplicationConfiguration config = new GwtApplicationConfiguration(600, 600);
+        config.antialiasing = true;
+        return config;
+    }
 
-        // USE THIS CODE FOR A FIXED SIZE APPLICATION
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
-        // END CODE FOR FIXED SIZE APPLICATION
-
-        // UNCOMMENT THIS CODE FOR A RESIZABLE APPLICATION
-        // PADDING is to avoid scrolling in iframes, set to 20 if you have problems
-        // private static final int PADDING = 0;
-        // private GwtApplicationConfiguration cfg;
-        //
-        // @Override
-        // public GwtApplicationConfiguration getConfig() {
-        //     int w = Window.getClientWidth() - PADDING;
-        //     int h = Window.getClientHeight() - PADDING;
-        //     cfg = new GwtApplicationConfiguration(w, h);
-        //     Window.enableScrolling(false);
-        //     Window.setMargin("0");
-        //     Window.addResizeHandler(new ResizeListener());
-        //     cfg.preferFlash = false;
-        //     return cfg;
-        // }
-        //
-        // class ResizeListener implements ResizeHandler {
-        //     @Override
-        //     public void onResize(ResizeEvent event) {
-        //         int width = event.getWidth() - PADDING;
-        //         int height = event.getHeight() - PADDING;
-        //         getRootPanel().setWidth("" + width + "px");
-        //         getRootPanel().setHeight("" + height + "px");
-        //         getApplicationListener().resize(width, height);
-        //         Gdx.graphics.setWindowedMode(width, height);
-        //     }
-        // }
-        // END OF CODE FOR RESIZABLE APPLICATION
-
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new MyGame();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new MyGame();
+    }
 }
