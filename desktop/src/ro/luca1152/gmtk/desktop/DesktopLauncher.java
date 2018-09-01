ba99e11f2be4afc5cb9f1a9669333a350cf81be6
Luca1152 <t.luca1152@gmail.com>
2018-09-01 13:34:21 +0300

    Initial commit.

---
+++ desktop/src/ro/luca1152/gmtk/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ro.luca1152.gmtk.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ro.luca1152.gmtk.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
