29c476a9a8148afca1b5638383b328c77a894281
Luca1152 <t.luca1152@gmail.com>
2018-09-06 16:45:17 +0300

    Added GNU GPL v3.0 license.

--- desktop/src/ro/luca1152/gmtk/desktop/DesktopLauncher.java
+++ desktop/src/ro/luca1152/gmtk/desktop/DesktopLauncher.java
@@ -1,3 +1,20 @@
+/*
+    This file is part of Gravity Box.
+
+    Gravity Box is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gravity Box is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gravity Box.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ro.luca1152.gmtk.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
